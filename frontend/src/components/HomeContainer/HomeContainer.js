bc6a85a77a8a1f7637a532056dfebe86cdfc8a70
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-03-14 03:38:53 +0100

    Implemented basic footer and favicon

---
+++ frontend/src/components/HomeContainer/HomeContainer.js
@@ -0,0 +1,11 @@
+import './HomeContainer.css';
+
+const HomeContainer = () => {
+  return (
+    <div className='home-container'>
+      <div></div>
+    </div>
+  );
+};
+
+export default HomeContainer;
--- frontend/src/views/App/App.js
+++ frontend/src/views/App/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
+import HomeContainer from '../../components/HomeContainer/HomeContainer';
+import Footer from '../../components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <NavBar />
+      <HomeContainer />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
