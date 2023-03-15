f714546ba44b0c7c8b86113067a28db48951d03c
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-03-15 14:24:23 +0100

    Implemented static lesson template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '@views/Home/Home';
 import Courses from './views/Courses/Courses';
 import Maths from './views/Maths/view/Maths';
+import Lesson1 from './views/Maths/lessons/Lesson1/Lesson1';
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Route path='/' element={<Home />} />
 			<Route path='/courses' element={<Courses />} />
 			<Route path='/courses/maths' element={<Maths />} />
+			<Route path='/courses/maths/lesson1' element={<Lesson1 />} />
 		</Routes>
 	);
 };
--- frontend/src/views/Maths/view/Maths.tsx
+++ frontend/src/views/Maths/view/Maths.tsx
@@ -16,10 +16,17 @@ const Maths = () => {
 			<div className={style['maths-content']}>
 				<span className={style['maths-span']}>Dostępne lekcje:</span>
 				<div className={style['maths-list']}>
-					<ThinTile name='1. Twierdzenie Pitagorasa' />
-					<ThinTile name='2. Funkcja kwadratowa' />
-					<ThinTile name='3. Logarytmy' />
-					<ThinTile name='4. Funkcje trygonometryczne' />
+					<Link to='/courses/maths/lesson1'>
+						<ThinTile name='1. Wzory skróconego mnożenia' />
+					</Link>
+					<ThinTile name='2. Twierdzenie Pitagorasa' />
+					<ThinTile name='3. Funkcja kwadratowa' />
+					<ThinTile name='4. Logarytmy' />
+					<ThinTile name='5. Funkcje trygonometryczne' />
+					<ThinTile name='6. Permutacje' />
+					<ThinTile name='7. Wartość bezwzględna' />
+					<ThinTile name='8. Ciąg arytmetyczny' />
+					<ThinTile name='9. Ciąg geometryczny' />
 				</div>
 			</div>
 		</div>
