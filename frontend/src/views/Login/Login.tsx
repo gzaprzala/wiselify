48be461fb9534e408db31a5762e92a96b896c199
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-03-20 20:32:29 +0100

    Added first version of login and register views

--- frontend/src/views/Login/Login.tsx
+++ frontend/src/views/Login/Login.tsx
@@ -1,13 +1,42 @@
+import Footer from '@/components/Footer/Footer';
+import MobileLogo from '@/components/MobileLogo/MobileLogo';
 import NavBar from '@/components/NavBar/NavBar';
+import { Link } from 'react-router-dom';
 import style from './Login.module.css';
 
 const Login = () => {
-	return (
-		<div className={style['login-container']}>
-			<NavBar />
-			<div className={style['login-container-content']}></div>
-		</div>
-	);
+  return (
+    <div className={style['login-container']}>
+      <NavBar />
+      <div className={style['login-container-content']}>
+        <MobileLogo />
+        <span className={style['login-container-content-span']}>Logowanie</span>
+        <form className={style['login-container-content-form']} action=''>
+          <input
+            className={style['login-container-content-form-input']}
+            type='text'
+            placeholder='login'
+          />
+          <input
+            className={style['login-container-content-form-input']}
+            type='password'
+            placeholder='hasło'
+          />
+          <input
+            className={style['login-container-content-form-button']}
+            type='button'
+            value='Zaloguj'
+          />
+        </form>
+        <Link to='/register'>
+          <span className={style['login-container-content-span-mini']}>
+            Zarejestruj się
+          </span>
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/views/Register/Register.tsx
+++ frontend/src/views/Register/Register.tsx
@@ -1,10 +1,52 @@
+import Footer from '@/components/Footer/Footer';
+import MobileLogo from '@/components/MobileLogo/MobileLogo';
 import NavBar from '@/components/NavBar/NavBar';
+import { Link } from 'react-router-dom';
 import style from './Register.module.css';
 
 const Register = () => {
   return (
     <div className={style['register-container']}>
       <NavBar />
+      <div className={style['register-container-content']}>
+        <MobileLogo />
+        <span className={style['register-container-content-span']}>
+          Rejestracja
+        </span>
+        <form className={style['register-container-content-form']} action=''>
+          <input
+            className={style['register-container-content-form-input']}
+            type='text'
+            placeholder='login'
+          />
+          <input
+            className={style['register-container-content-form-input']}
+            type='text'
+            placeholder='email'
+          />
+          <input
+            className={style['register-container-content-form-input']}
+            type='password'
+            placeholder='hasło'
+          />
+          <input
+            className={style['register-container-content-form-input']}
+            type='password'
+            placeholder='powtórz hasło'
+          />
+          <input
+            className={style['register-container-content-form-button']}
+            type='button'
+            value='Zarejestruj'
+          />
+        </form>
+        <Link to='/login'>
+          <span className={style['register-container-content-span-mini']}>
+            Zaloguj się
+          </span>
+        </Link>
+      </div>
+      <Footer />
     </div>
   );
 };
